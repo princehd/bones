7f6c0d059910f4ee917867ead47c4c2bf835deba
dannsgo <dannsgo@gmail.com>
2022-02-28 14:01:51 +0900

    회원가입 html/css

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 // input > date
-document.getElementById('nav_date').value = new Date().toISOString().substring(0, 10);
+document.getElementById('nav_date').value = new Date()
+  .toISOString()
+  .substring(0, 10);
 // tab menu
 const tabList = document.querySelectorAll('.tab_menu .tab_btn li');
-for(let i = 0; i < tabList.length; i++){
-  tabList[i].querySelector('.tab_btns').addEventListener('click', function(changeTab){
-    changeTab.preventDefault();
-    for(let j = 0; j < tabList.length; j++){
-      tabList[j].classList.remove('tab_menus');
-    }
-    this.parentNode.classList.add('tab_menus');
-  });
+for (let i = 0; i < tabList.length; i++) {
+  tabList[i]
+    .querySelector('.tab_btns')
+    .addEventListener('click', function (changeTab) {
+      changeTab.preventDefault();
+      for (let j = 0; j < tabList.length; j++) {
+        tabList[j].classList.remove('tab_menus');
+      }
+      this.parentNode.classList.add('tab_menus');
+    });
 }
 
 // Canvas
-const canvas = document.getElementById("boneCanvas");
+const canvas = document.getElementById('boneCanvas');
 // Pixel Control == 2D
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 
 // canvas.width = ;
 // canvas.height = ;
