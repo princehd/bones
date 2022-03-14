753100e45ad04dc9528949a51936874cfc9b3d45
PHyeonMIN <dndb4902@naver.com>
2022-03-14 11:22:26 +0900

    p

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-// import Join from "./components/Join";
-// import Login from "./components/Login";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Join from "./components/Join";
+import Login from "./components/Login";
 import Main from "./components/Main";
 
 function App() {
     return (
         <div>
-            {/* <Join /> */}
-            {/* <Login /> */}
-            <Main />
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path="/" element={<Login />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/join" element={<Join />} />
+                    <Route path="/main" element={<Main />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
